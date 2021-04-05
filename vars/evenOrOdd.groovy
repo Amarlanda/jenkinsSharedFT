9ae562083c2bd29581b7555673bee9ed4654b606
amar landa <al@amarlanda.com>
2021-04-05 23:28:58 +0100

    new test

--- vars/evenOrOdd.groovy
+++ vars/evenOrOdd.groovy
@@ -17,8 +17,8 @@ def call(int buildNumber) {
         stage('Even Stage') {
           steps {
             container('terraform'){
-              sh ' terraform init'
-              sh 'hostname'
+              sh " terraform init"
+              sh "hostname"
             }
 
             /*         
