1189e8d66f4e92f233ccb20adc3851b2b600ff28
amar landa <al@amarlanda.com>
2021-04-07 22:59:35 +0100

    basically fixed service account and workload identity

--- vars/evenOrOdd.groovy
+++ vars/evenOrOdd.groovy
@@ -18,7 +18,7 @@ def call(int buildNumber) {
           steps {
             container('gcloud'){
 
-              sh "gcloud container clusters get-credentials prod-trading-clus01 --region europe-west2 --project avian-mile-300915"
+              sh "gcloud container clusters get-credentials prod-trading-clus01 --region europe-west2 --project ons002"
               sh "kubectl get svc"
               sh "ls /home/jenkins/agent"
               sh "ls /home/jenkins/"
